2085cbcc0bc199b8232443726c1b582f70fbd9d5
Thoai Do Huy <thoaidohuy@gmail.com>
2021-03-15 12:14:35 +0700

    Add project files.

---
+++ Assessment 2 Flip Cards/Card.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_2_Flip_Cards
+{
+    class Card
+    {
+        private string CardName;
+        private string Definition;
+        private bool Flipped;
+
+
+        public Card(string name, string definition)
+        {
+            CardName = name;
+            Definition = definition;
+            Flipped = false;
+        }
+
+        public string GetText()
+        {
+            if (Flipped)
+            {
+                return CardName;
+            }
+            else if (!Flipped)
+            {
+                return Definition;
+            }
+        }
+        public bool IsFlipped()
+        {
+            return Flipped;
+        }
+        public void FlipCard()
+        {
+            if (Flipped)
+            {
+                Flipped = false;
+            }
+            else if(!Flipped)
+            {
+                Flipped = true;
+            }
+        }
+
+    }
+}
---
+++ Assessment 2 Flip Cards/Deck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_2_Flip_Cards
+{
+    class Deck
+    {
+        private Card[] Cards;
+        private string FileName;
+
+        public Deck(string FileName)
+        {
+
+        }
+
+
+
+    }
+}
